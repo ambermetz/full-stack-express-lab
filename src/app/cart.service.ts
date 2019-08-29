46e157b4d53a1dcfad8ebda3cda13a0b11045688
Amber Metz <ambermetz88@gmail.com>
2019-08-29 08:57:45 -0400

    Part 2 of lab

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -11,4 +11,16 @@ export class CartService {
   getAllItems(): Observable<any> {
     return this.http.get("http://localhost:8080/cart-items");
   }
+
+  addItem(item: object): Observable<any> {
+    return this.http.post("http://localhost:8080/cart-items", item);
+  }
+
+  updateItem(updateItem: object, id: number): Observable<any> {
+    return this.http.put(`http://localhost:8080/cart-items/${id}`, updateItem);
+  }
+
+  removeItem(id: number): Observable<any> {
+    return this.http.delete(`http://localhost:8080/cart-items/${id}`);
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,11 +11,46 @@ import { Observable } from "rxjs";
 })
 export class ProductsComponent implements OnInit {
   cart: any[];
+  show: boolean = false;
+  itemEdit: object;
   constructor(private http: HttpClient, private cartService: CartService) {}
 
   ngOnInit() {
     this.cartService
       .getAllItems()
       .subscribe(response => (this.cart = response));
+  }
+
+  addItems(form: NgForm): void {
+    this.cartService.addItem(form.value).subscribe(response => {
+      this.cart = response;
+      // form.reset();
+    });
+  }
+
+  setItemsToEdit(item: object): void {
+    this.itemEdit = item;
+    console.log(this.itemEdit);
+  }
+
+  updateItems(form: NgForm, id: number): void {
+    // console.log(form);
+    // console.log(id);
+    this.cartService.updateItem(form.value, id).subscribe(response => {
+      this.cart = response;
+    });
+    this.show = !this.show;
+  }
+
+  toggleForm(): void {
+    this.show = !this.show;
+    console.log("this was clicked");
+  }
+
+  deleteItem(id: number): void {
+    console.log("this was clicked");
+    this.cartService.removeItem(id).subscribe(response => {
+      this.cart = response;
+    });
   }
 }
